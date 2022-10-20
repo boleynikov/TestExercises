f9a8848034fa109d7b5838bbaeb9617cf0efed80
oleinikov.bogdan <olejnikov.bogdan.s@hneu.net>
2022-10-20 14:31:13 +0300

    completed task 2

--- Task_2/SQLQuery.sql
+++ Task_2/SQLQuery.sql
@@ -1,4 +1,4 @@
---3.1
+/*3.1*/
 Select NTOV, 
        Sum(Kol) as SumKol, 
 	   Sum(Kol*Cena) as SumMoney 
@@ -9,14 +9,14 @@ where DMZ.PR = 2 and DMZ.DDM = '2020-01-04'
 group by NTOV
 order by SumMoney desc
 
---3.2
+/*3.2*/
 Update DMS
 	Set DMS.SORT = TOV.SORT 
 from DMS 
 	join TOV on DMS.KTOV = TOV.KTOV
 
 
---3.3
+/*3.3*/
 Select NTOV, 
        SUM(KOL*(3-PR)-KOL*PR) as Rem, 
 	   SUM((KOL*(3-PR)-KOL*PR) * DMS.CENA) as RemMoney 
@@ -27,18 +27,41 @@ Group by NTOV
 Order by NTOV
 
 
---3.4
-Declare @prihod INT,
+/*3.4*/
+Declare @newNdm INT,
+        @prihod INT,
 		@rashod INT,
 		@newPR INT
 Set @prihod = (Select Count(*) from DMZ Where PR = 1);
 Set @rashod = (Select Count(*) from DMZ Where PR = 2);
-IF @prihod > @rashod
+IF EXISTS(Select * from DMZ)
+	Set @newNdm = (Select MAX(NDM) from DMZ) + 1
+ELSE
+	Set @newNdm = 1
+IF @prihod >= @rashod
 	Set @newPR = 2
 ELSE
 	Set @newPR = 1
+
 Insert into DMZ 
-Select MAX(NDM) + 1, 
-       GETDATE(), 
+Select @newNdm, 
+	   GETDATE(), 
 	   @newPR
-from DMZ
+
+/*3.5*/
+Declare @min INT,
+	    @max INT
+Set @min = (Select MIN(NDM) from DMZ)
+Set @max = (Select MAX(NDM) from DMZ)
+
+Insert into DMS
+Select @max, 
+       KTOV, 
+	   KOL, 
+	   CENA, 
+	   SORT 
+from DMS
+Where NDM = @min and 
+      KTOV Not In (Select KTOV 
+	               from DMS 
+				   Where NDM = @max)
